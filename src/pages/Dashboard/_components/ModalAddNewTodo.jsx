9f2d8c7d87579aa16a532e57b1f78695b6eeeca6
Arga <argaprataman052@gmail.com>
2024-04-07 14:43:29 +0700

    form

--- src/pages/Dashboard/_components/ModalAddNewTodo.jsx
+++ src/pages/Dashboard/_components/ModalAddNewTodo.jsx
@@ -1,10 +1,34 @@
 import React from "react";
 import { Modal } from "antd";
+import styled from "styled-components";
+
+const Input = styled.input`
+  padding: 10px;
+  background-color: #fff;
+  outline: none;
+  border: 0;
+  border-radius: 10px;
+`;
+
+const Button = styled.button`
+  padding: 10px;
+  background-color: blue;
+  border: 0;
+  color: #fff;
+`;
 
 const ModalAddNewTodo = ({ onCancel }) => {
+
+  const handsubmit = (e) => {
+
+  }
+
   return (
     <Modal open title="Add New Todo" onCancel={onCancel}>
-      <p>This is Modal</p>
+      <form onSubmit={handsubmit}>
+        <Input type="text" placeholder="what your message" />
+        <Button type="submit">Kirim</Button>
+      </form>
     </Modal>
   );
 };
